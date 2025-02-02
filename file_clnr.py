ef7c914e46e221945c57e5a3b9ffeee0eab3f1e4
RodHiz <24rhizmeriseguel@liverpoolmathsschool.org>
2025-02-02 19:10:22 +0000

    Update data loading process; switch from CSV to Excel input and adjust output file naming

--- file_clnr.py
+++ file_clnr.py
@@ -1,14 +1,14 @@
 import pandas as pd
 
 # Load datasets
-temp_df = pd.read_csv(r"C:\Users\rodri\OneDrive\Documents\School - ULMaS\Industry Project - Yozu\researchFiles_temp\leedsTempConditions.csv")
-precip_df = pd.read_csv(r"C:\Users\rodri\OneDrive\Documents\School - ULMaS\Industry Project - Yozu\researchFiles_temp\leedsPrecipQuant.csv")
+temp_df = pd.read_csv(r"C:\Users\rodri\OneDrive\Documents\School - ULMaS\Industry Project - Yozu\researchFiles_temp\network 8-10-24 CLEAN.xlsx")
+#precip_df = pd.read_csv(r"C:\Users\rodri\OneDrive\Documents\School - ULMaS\Industry Project - Yozu\researchFiles_temp\leedsPrecipQuant.csv")
 
 # Merge on the 'date' column using a left join
-merged_df = temp_df.merge(precip_df, on="datetime", how="left")
+#merged_df = temp_df.merge(precip_df, on="datetime", how="left")
 
 # Save the merged dataset
-merged_df.to_csv("leedsWeather.csv", index=False)
+temp_df.to_csv("networkCLEAN.csv", index=False)
 
 # Display the first few rows
-print(merged_df.head())
+print(temp_df.head())
